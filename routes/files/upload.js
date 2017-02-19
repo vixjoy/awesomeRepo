3678a6b81706aa6ed8c14ef1127eb1c69f7d7490
Krishna Glick <unstoppable411@gmail.com>
2017-02-18 20:59:17 -0500

    hur error handling

--- routes/files/upload.js
+++ routes/files/upload.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const boom = require('boom');
 
-exports.handler = function(req, rep) {
+exports.handler = async function(req, rep) {
   const { token } = req.state;
   const { helpers } = req.server.app;
   const data = req.payload;
@@ -12,6 +12,7 @@ exports.handler = function(req, rep) {
 
   const { username } = helpers.decryptToken(token);
   if (fileName && data[fileName]) {
+    await new Promise((res) => fs.mkdir('../../uploads/', res));
     const savePath = path.resolve(__dirname + '/../../uploads/' + fileName + '.archive');
     const file = fs.createWriteStream(savePath);
 
