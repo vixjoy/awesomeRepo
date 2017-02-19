32f9683f0964c140a2e8b17c5d5e4d403f0c1bab
Krishna Glick <unstoppable411@gmail.com>
2017-02-19 01:08:34 -0500

    improvements

--- routes/files/delete.js
+++ routes/files/delete.js
@@ -1,12 +1,12 @@
 
 exports.handler = async function(req, rep) {
   const { helpers } = req.server.app;
-  const { id } = req.params;
+  const { id } = req.payload;
   const { token } = req.state;
   const { username } = helpers.decryptToken(token);
   rep(await helpers.deleteFile(username, id));
 };
 
-exports.method = 'GET';
+exports.method = 'POST';
 
 exports.pres = ['authenticateUser'];
